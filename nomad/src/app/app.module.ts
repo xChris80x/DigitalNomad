3e72c65173b81ebaaf0663c698177980d1bd00e7
Chris <1chris-schroeder@web.de>
2023-02-20 20:29:24 +0100

    MainMaps

--- nomad/src/app/app.module.ts
+++ nomad/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
    ContentComponent,
-   FooterComponent
+   FooterComponent,
+   MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ nomad/src/app/map/map.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  map!: L.Map;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(this.map);
+  }
+
+}
